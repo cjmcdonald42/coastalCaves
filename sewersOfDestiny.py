d1ba7d60415758fdd8acb4f175cff80cf94eba32
Chuck <cmcdonald@daviestech.org>
2026-03-19 13:06:08 -0400

    removed ability scores, simplify

---
+++ sewersOfDestiny.py
@@ -0,0 +1,26 @@
+#         package: sewersOfDestiny.py
+#          author: Charles J McDonald «https://github.com/cjmcdonald42»
+#    date written: 2026.03.19
+#     description: A sample adventure game for learning to code in python.
+
+# Introduction, describe the scene as our hero delves deep into the city sewers.
+print("""
+    Our adventure begins with you heading into the city sewers, old, cobblestone tunnels rife with an aroma
+    so thick you stand a fork in it. From the moment you enter, you are engulfed by the smell of a heavy rot
+    that tastes like wet earth and ancient iron. It’s not just a smell; but a presence that clings to the back
+    of your throat.
+
+    The walls are a masterpiece of grime and slick undulate. Jagged cobblestones, slick with a bioluminescent
+    sheen of black algae, seem to pulse under the torchlight like the scales of a buried beast. Every footstep
+    on the sodden floor is answered by a hollow, rhythmic dripping from the darkness ahead, echoing with a metallic
+    ping suggests the pipes carrying something far denser than water.
+
+    As you move deeper into the sewers, the city sounds above fade into a dead, crushing silence, replaced by
+    the low, guttural groan of shifting stonework.
+    """)
+
+# Face a challenge --> Fight, Magic, Escape
+
+# Challenge retaliates
+
+# Resolution?
