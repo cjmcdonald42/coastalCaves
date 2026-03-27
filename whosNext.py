4a87fe9234d0826b6372ea7c14976b924978e561
Chuck <cmcdonald@daviestech.org>
2026-03-27 10:59:54 -0400

    updates to code

---
+++ whosNext.py
@@ -0,0 +1,13 @@
+"""
+         package: whosNext.py
+          author: Charles J McDonald «https://github.com/cjmcdonald42»
+    last updated: 2026.03.27
+     description: Pick the next student who will present their work.
+
+"""
+
+# Create a list of students
+# Ask the teacher to spin the wheel
+# Randomly choose the next student
+# Remove that student from the list
+# Ask the teacher to choose next or end
